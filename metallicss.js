64c716734bed57ae21a1a2ce69b536ffa05e2e27
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2022-12-07 23:43:01 +0100

    Rendering improvements

--- metallicss.js
+++ metallicss.js
@@ -1,32 +1,6 @@
 const base = [
     tag("rect", {
-      props: {
-        fill: "url(#gradient)",
-        height: 1024,
-        width: 1024,
-        x: 0,
-        y: 0,
-      },
-    }),
-    tag("rect", {
-      props: {
-        filter: "url(#texture)",
-        height: 1024,
-        style: "mix-blend-mode: luminosity; opacity: 0.5",
-        width: 1024,
-        x: 0,
-        y: 0,
-      },
-    }),
-    tag("rect", {
-      props: {
-        filter: "url(#texture)",
-        height: 1024,
-        style: "mix-blend-mode: screen; opacity: 0.25",
-        width: 1024,
-        x: 0,
-        y: 0,
-      },
+      props: { fill: "url(#gradient)", height: 1024, width: 1024, x: 0, y: 0 },
     }),
   ],
   defs = tag("defs", {
@@ -49,9 +23,6 @@ const base = [
   serializer = new XMLSerializer(),
   xmlns = "http://www.w3.org/2000/svg",
   domUrl = window.URL || window.webkitURL || window;
-let blocked =
-  document.body.className.indexOf("block-metallicss") !== -1 ||
-  document.body.querySelectorAll(".block-metallicss").length > 0;
 
 function tag(name, { inner, props }) {
   const elem = document.createElementNS("http://www.w3.org/2000/svg", name);
@@ -86,7 +57,7 @@ function rasterify(elem, svg, callback) {
   const url = domUrl.createObjectURL(
       new Blob([svg], { type: "image/svg+xml;charset=utf-8" })
     ),
-    image = new Image();
+    image = callback ? new Image() : elem.querySelector(".metal");
 
   image.onload = function () {
     const canvas = document.createElement("canvas"),
@@ -96,21 +67,16 @@ function rasterify(elem, svg, callback) {
     canvas.height = 512;
     context.drawImage(this, 0, 0);
     domUrl.revokeObjectURL(url);
-    if (callback) callback(canvas.toDataURL());
-    else elem.style.backgroundImage = `url(${canvas.toDataURL()})`;
+    if (callback) {
+      image.remove();
+      callback(canvas.toDataURL());
+    }
     canvas.remove();
-    image.remove();
   };
   image.src = url;
 }
 
-export const block = () => {
-    blocked = true;
-  },
-  unblock = () => {
-    blocked = false;
-  },
-  metallicss = (elem) => {
+export const metallicss = (elem) => {
     const { offsetWidth: width, offsetHeight: height } = elem,
       { backgroundColor: background, borderRadius } = getComputedStyle(elem),
       depthValue = getComputedStyle(elem).getPropertyValue("--convexity"),
@@ -151,8 +117,100 @@ export const block = () => {
         .replace(/ /g, "")
         .split(",")
         .map((str) => (parseInt(str) / 256).toFixed(3)),
-      matrix = `${rgb[0]} 0 0 0 0 0 ${rgb[1]} 0 0 0 0 0 ${rgb[2]} 0 0 0 0 0 1 0`;
+      backdrop = elem.querySelector(".metal");
 
+    if (width === 0 || height === 0) return;
+    if (backdrop === null) {
+      const image = new Image();
+
+      image.className = "metal";
+      elem.append(image);
+    }
+    if (
+      document.getElementById(
+        `svg_for_filter_lustre_${fill.replace(/ /g, "")}_${seed}`
+      ) === null
+    )
+      document.body.append(
+        tag("svg", {
+          props: {
+            id: `svg_for_filter_lustre_${fill.replace(/ /g, "")}_${seed}`,
+            width: 0,
+            height: 0,
+          },
+          inner: [
+            tag("filter", {
+              inner: [
+                tag("feColorMatrix", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    in: "SourceGraphic",
+                    type: "matrix",
+                    values: `${rgb[0]} 0 0 0 0 0 ${rgb[1]} 0 0 0 0 0 ${rgb[2]} 0 0 0 0 0 1 0`,
+                    result: "color",
+                  },
+                }),
+                tag("feComponentTransfer", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    in: "color",
+                    result: "brightness",
+                  },
+                  inner: [
+                    tag("feFuncR", {
+                      props: { type: "linear", slope: "2" },
+                    }),
+                    tag("feFuncG", {
+                      props: { type: "linear", slope: "2" },
+                    }),
+                    tag("feFuncB", {
+                      props: { type: "linear", slope: "2" },
+                    }),
+                  ],
+                }),
+                tag("feBlend", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    mode: "color",
+                    in: "color",
+                    in2: "SourceGraphic",
+                    result: "colorized",
+                  },
+                }),
+                tag("feTurbulence", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    baseFrequency: 0.005,
+                    result: "turbulence",
+                    seed,
+                    type: "turbulence",
+                  },
+                }),
+                tag("feBlend", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    mode: "screen",
+                    in: "turbulence",
+                    in2: "colorized",
+                    result: "randomized",
+                  },
+                }),
+                tag("feBlend", {
+                  props: {
+                    ["color-interpolation-filters"]: "sRGB",
+                    mode: "soft-light",
+                    in: "brightness",
+                    in2: "randomized",
+                  },
+                }),
+              ],
+              props: {
+                id: `lustre_${fill.replace(/ /g, "")}_${seed}`,
+              },
+            }),
+          ],
+        })
+      );
     rasterify(
       elem,
       serializer.serializeToString(
@@ -204,9 +262,9 @@ export const block = () => {
                         ["color-interpolation-filters"]: "sRGB",
                         in: "image",
                         result: "blur",
-                        stdDeviation: `${scale * (y / x) || 0},${
-                          scale * 1.5 * (x / y) || 0
-                        }`,
+                        stdDeviation: `${parseInt(
+                          scale * (y / x) || 0
+                        )},${parseInt(scale * 1.5 * (x / y) || 0)}`,
                       },
                     }),
                     tag("feDisplacementMap", {
@@ -233,110 +291,6 @@ export const block = () => {
                     y: 0,
                   },
                 }),
-                tag("filter", {
-                  inner: [
-                    tag("feTurbulence", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        baseFrequency: 0.005,
-                        result: "turbulence",
-                        seed,
-                        type: "fractalNoise",
-                      },
-                    }),
-                    tag("feDisplacementMap", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        in2: "turbulence",
-                      },
-                    }),
-                  ],
-                  props: {
-                    filterUnits: "userSpaceOnUse",
-                    height: 1024,
-                    id: "texture",
-                    width: 1024,
-                    x: 0,
-                    y: 0,
-                  },
-                }),
-                tag("filter", {
-                  inner: [
-                    tag("feTurbulence", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        baseFrequency: "0.0025,0.005",
-                        numOctaves: 3,
-                        type: "fractalNoise",
-                        stitchTiles: "stitch",
-                        result: "grain",
-                        seed,
-                      },
-                    }),
-                    tag("feDisplacementMap", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        in: "SourceGraphic",
-                        in2: "grain",
-                        scale: 5,
-                      },
-                    }),
-                  ],
-                  props: {
-                    id: "grain",
-                  },
-                }),
-                tag("filter", {
-                  inner: [
-                    tag("feColorMatrix", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        in: "SourceGraphic",
-                        type: "matrix",
-                        values: matrix,
-                        result: "color",
-                      },
-                    }),
-                    tag("feComponentTransfer", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        in: "color",
-                        result: "brightness",
-                      },
-                      inner: [
-                        tag("feFuncR", {
-                          props: { type: "linear", slope: "2" },
-                        }),
-                        tag("feFuncG", {
-                          props: { type: "linear", slope: "2" },
-                        }),
-                        tag("feFuncB", {
-                          props: { type: "linear", slope: "2" },
-                        }),
-                      ],
-                    }),
-                    tag("feBlend", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        mode: "color",
-                        in: "color",
-                        in2: "SourceGraphic",
-                        result: "colorized",
-                      },
-                    }),
-                    tag("feBlend", {
-                      props: {
-                        ["color-interpolation-filters"]: "sRGB",
-                        mode: "soft-light",
-                        in: "brightness",
-                        in2: "colorized",
-                      },
-                    }),
-                  ],
-                  props: {
-                    id: "lustre",
-                  },
-                }),
                 defs,
                 tag("radialGradient", {
                   inner: [
@@ -351,34 +305,21 @@ export const block = () => {
                       props: {
                         offset: "67%",
                         ["stop-color"]: "black",
-                        ["stop-opacity"]: inverse ? 0.5 : 0.25,
+                        ["stop-opacity"]: 0.5,
                       },
                     }),
                   ],
                   props: { id: "radial" },
                 }),
-                tag("g", {
-                  inner: [
-                    tag("g", {
-                      inner: [
-                        tag("g", {
-                          inner: base,
-                          props: { filter: "url(#noise)" },
-                        }),
-                        tag("rect", {
-                          props: {
-                            fill: "url(#radial)",
-                            height: 1024,
-                            width: 1024,
-                            x: 0,
-                            y: 0,
-                          },
-                        }),
-                      ],
-                      props: { filter: "url(#grain)" },
-                    }),
-                  ],
-                  props: { filter: "url(#lustre)" },
+                tag("g", { inner: base, props: { filter: "url(#noise)" } }),
+                tag("rect", {
+                  props: {
+                    fill: "url(#radial)",
+                    height: 1024,
+                    width: 1024,
+                    x: 0,
+                    y: 0,
+                  },
                 }),
               ],
               props: {
@@ -394,39 +335,39 @@ export const block = () => {
         )
     );
     elem.style.backgroundSize = "100% 100%";
-    elem.style.border = "none";
     elem.style.boxShadow = inverse
       ? ""
       : "#00000030 1px 2px 2px, #00000020 2px 4px 4px";
-    elem.style.color = `#${
-      (inverse || height > 100) && elem.innerText !== "MetalliCSS"
-        ? `${
-            {
-              copper: "100000",
-              gold: "302000",
-              neutral: "000000",
-              silver: "001010",
-              lead: "000000",
-            }[metal || "neutral"]
-          }c0`
-        : "ffffffe0"
+    elem.style.color = `${
+      inverse || (height > 100 && elem.innerText !== "MetalliCSS")
+        ? "black"
+        : "white"
     }`;
     elem.style.textRendering = "geometricPrecision";
     elem.style.textShadow =
-      (inverse || height > 100) && elem.innerText !== "MetalliCSS"
+      inverse || (height > 100 && elem.innerText !== "MetalliCSS")
         ? "white .5px .5px 1px"
         : "black -.5px -.5px 1px";
     elem.style.transform = "translateZ(0)";
-    elem.style.transition = "none";
+    elem.style.overflow = "hidden";
+    if (backdrop) {
+      backdrop.style.filter = "";
+      setTimeout(() => {
+        backdrop.style.filter = `url("#lustre_${fill.replace(
+          / /g,
+          ""
+        )}_${seed}")`;
+      }, 0);
+      backdrop.style.position = "absolute";
+      backdrop.style.top = "0";
+      backdrop.style.left = "0";
+      backdrop.style.zIndex = "-1";
+      backdrop.style.width = "100%";
+      backdrop.style.height = "100%";
+    }
   },
   traverse = () =>
-    !blocked &&
-    Array.from(document.querySelectorAll(".metallicss"))
-      .filter(
-        (elem) =>
-          window.getComputedStyle(elem).getPropertyValue("display") != "none"
-      )
-      .forEach(metallicss);
+    Array.from(document.querySelectorAll(".metallicss")).forEach(metallicss);
 
 if (document.readyState !== "loading") traverse();
 else document.addEventListener("DOMContentLoaded", traverse);
