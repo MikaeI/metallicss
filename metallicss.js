fd83b0e3c7f976310334d2fb236f98d4f7ac47b1
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2023-01-23 00:12:02 +0100

    Whole new look

--- metallicss.js
+++ metallicss.js
@@ -22,102 +22,20 @@ const serializer = new XMLSerializer(),
   isFirefox = window.navigator.userAgent.indexOf("Firefox") > -1,
   dims = { height: 2048, width: 2048 },
   sRGB = { ["color-interpolation-filters"]: "sRGB" },
-  randomSeed = Math.floor(Math.random() * 100) + 1,
-  gradients = [
-    tag("radialGradient", {
-      inner: [
-        tag("stop", { props: { ["stop-color"]: "#c0c0d0", offset: 0 } }),
-        tag("stop", { props: { ["stop-color"]: "#606070", offset: 1 } }),
-      ],
-      props: { ["color-interpolation"]: "sRGB", cx: 0.6, cy: 1, id: "n" },
-    }),
-    tag("radialGradient", {
-      inner: [
-        tag("stop", { props: { ["stop-color"]: "#404040", offset: 0 } }),
-        tag("stop", { props: { ["stop-color"]: "white", offset: 1 } }),
-      ],
-      props: { ["color-interpolation"]: "sRGB", cx: 0.6, cy: 0, id: "s" },
-    }),
-  ],
-  map = tag("feImage", {
+  map = await tag("feImage", {
     props: {
       ...dims,
       ...sRGB,
-      href: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(
-        serializer.serializeToString(
-          tag("svg", {
-            inner: [
-              tag("linearGradient", {
-                inner: [
-                  tag("stop", {
-                    props: {
-                      ["stop-color"]: "#ff0000",
-                      ["stop-opacity"]: 1,
-                      offset: 0,
-                    },
-                  }),
-                  tag("stop", {
-                    props: {
-                      ["stop-color"]: "#ff0000",
-                      ["stop-opacity"]: 0,
-                      offset: 1,
-                    },
-                  }),
-                ],
-                props: {
-                  ["color-interpolation"]: "sRGB",
-                  id: "red",
-                  x1: 0,
-                  x2: 1,
-                  y1: 0,
-                  y2: 0,
-                },
-              }),
-              tag("linearGradient", {
-                inner: [
-                  tag("stop", {
-                    props: {
-                      ["stop-color"]: "#0000ff",
-                      ["stop-opacity"]: 1,
-                      offset: 0,
-                    },
-                  }),
-                  tag("stop", {
-                    props: {
-                      ["stop-color"]: "#0000ff",
-                      ["stop-opacity"]: 0,
-                      offset: 1,
-                    },
-                  }),
-                ],
-                props: {
-                  ["color-interpolation"]: "sRGB",
-                  id: "blue",
-                  x1: 0,
-                  x2: 0,
-                  y1: 0,
-                  y2: 1,
-                },
-              }),
-              tag("rect", {
-                props: { fill: "black", height: "100%", width: "100%" },
-              }),
-              tag("rect", {
-                props: { fill: "url(#red)", height: "100%", width: "100%" },
-              }),
-              tag("rect", {
-                props: {
-                  fill: "url(#blue)",
-                  height: "100%",
-                  width: "100%",
-                  style: "mix-blend-mode: screen",
-                },
-              }),
-            ],
-            props: { ...dims, xmlns },
-          })
-        )
-      )}`,
+      href: await fetch("./map.png")
+        .then((response) => response.blob())
+        .then((blob) => {
+          return new Promise((resolve) => {
+            const reader = new FileReader();
+
+            reader.onloadend = () => resolve(reader.result);
+            reader.readAsDataURL(blob);
+          });
+        }),
       preserveAspectRatio: "none",
       result: "map",
     },
@@ -126,47 +44,61 @@ const serializer = new XMLSerializer(),
     const { offsetHeight: y, offsetWidth: x } = elem,
       { backgroundColor: background, borderRadius } = getComputedStyle(elem),
       depth = getComputedStyle(elem).getPropertyValue("--convexity"),
-      seed = getComputedStyle(elem).getPropertyValue("--seed") || randomSeed,
-      metal = getComputedStyle(elem)
-        .getPropertyValue("--metal")
-        .replace(/ /g, ""),
       rawDepth =
         depth === "0" || depth === " 0" ? 0 : (depth && parseInt(depth)) || 4,
       absRawDepth = Math.abs(rawDepth),
       radius = parseInt(borderRadius),
       inverse = rawDepth < 0,
-      fill =
-        metal === "gold"
-          ? "rgb(255, 255, 0)"
-          : metal === "copper"
-          ? "rgb(255, 128, 0)"
-          : metal === "silver"
-          ? "rgb(255, 255, 255)"
-          : metal === "lead"
-          ? "rgb(0, 0, 0)"
-          : background === "rgba(0, 0, 0, 0)"
-          ? "rgb(128, 128, 128)"
-          : background,
       canvas =
         elem.querySelector(":scope > .metal") ||
         document.createElement("canvas"),
       context = canvas.getContext("2d", { alpha: false }),
+      finish = getComputedStyle(elem)
+        .getPropertyValue("--metal")
+        .replace(/ /g, ""),
+      fill =
+        finish === "gold"
+          ? "yellow"
+          : finish === "copper"
+          ? "rgb(128, 64, 0)"
+          : finish === "silver"
+          ? "rgb(192, 192, 192)"
+          : finish === "lead"
+          ? "rgb(0, 0, 0)"
+          : background === "rgba(0, 0, 0, 0)"
+          ? "rgb(128, 128, 128)"
+          : background,
       inner = [
-        tag("rect", { props: { fill: "url(#n)", height: 1200, width: 2048 } }),
-        tag("rect", {
-          props: { fill: "url(#s)", height: 848, width: 2048, y: 1200 },
+        tag("image", {
+          props: { href: metal, ...dims, preserveAspectRatio: "none" },
         }),
         tag("rect", {
           props: {
             ...dims,
             fill,
-            style: "mix-blend-mode: color",
+            style: "mix-blend-mode: luminosity",
             opacity: 0.25,
           },
         }),
-        tag("rect", { props: { ...dims, filter: "url(#perlin)" } }),
-      ],
-      noise = 0.0075 + (x * y) / 11520000 / 4; // 0.01
+        tag("rect", {
+          props: {
+            x: 512,
+            y: 512,
+            width: 1024,
+            height: 1024,
+            fill: "url(#color)",
+            style: "mix-blend-mode: color",
+          },
+        }),
+        tag("rect", {
+          props: {
+            ...dims,
+            fill,
+            opacity: 0.25,
+            style: "mix-blend-mode: overlay",
+          },
+        }),
+      ];
     let tempImage,
       url,
       rx =
@@ -199,7 +131,6 @@ const serializer = new XMLSerializer(),
           serializer.serializeToString(
             tag("svg", {
               inner: [
-                ...gradients,
                 tag("filter", {
                   inner: [
                     map,
@@ -265,64 +196,53 @@ const serializer = new XMLSerializer(),
                         yChannelSelector: "B",
                       },
                     }),
-                    tag("feComponentTransfer", {
-                      inner: "RGB".split("").map((channel) =>
-                        tag(`feFunc${channel}`, {
-                          props: {
-                            type: "linear",
-                            slope: "2",
-                            intercept: -0.5,
-                          },
-                        })
-                      ),
-                      props: sRGB,
-                    }),
-                    tag("feComponentTransfer", {
-                      inner: "RGB".split("").map((channel) =>
-                        tag(`feFunc${channel}`, {
-                          props: { type: "linear", slope: 1.1 },
-                        })
-                      ),
-                      props: sRGB,
-                    }),
-                    tag("feColorMatrix", {
-                      props: { ...sRGB, type: "saturate", values: 1.5 },
-                    }),
                   ],
                   props: { id: "warp", x: 0, y: 0 },
                 }),
-                tag("filter", {
+                tag("linearGradient", {
                   inner: [
-                    tag("feTurbulence", {
+                    tag("stop", {
                       props: {
-                        ...sRGB,
-                        baseFrequency: `${noise / 2} ${noise}`,
-                        seed,
-                        type: "fractalNoise",
+                        ["stop-color"]: fill,
+                        ["stop-opacity"]: 0.5,
+                        offset: 0.3,
                       },
                     }),
-                    tag("feColorMatrix", {
-                      props: { ...sRGB, type: "saturate", values: 0.1 },
+                    tag("stop", {
+                      props: {
+                        ["stop-color"]: fill,
+                        ["stop-opacity"]: 0.125,
+                        offset: 0.7,
+                      },
+                    }),
+                    tag("stop", {
+                      props: {
+                        ["stop-color"]: fill,
+                        ["stop-opacity"]: 0.5,
+                        offset: 1,
+                      },
                     }),
                   ],
-                  props: { id: "perlin" },
+                  props: {
+                    ["color-interpolation"]: "sRGB",
+                    gradientTransform: "rotate(90)",
+                    id: "color",
+                  },
                 }),
                 tag("g", {
                   inner: inverse
                     ? [
                         ...inner,
-                        tag("rect", { props: { ...dims, fill: "#30303030" } }),
+                        tag("rect", {
+                          props: {
+                            ...dims,
+                            fill: "#40404060",
+                            style: "mix-blend-mode: multiply",
+                          },
+                        }),
                       ]
                     : inner,
                   props: { filter: "url(#warp)" },
-                }),
-                tag("rect", {
-                  props: {
-                    ...dims,
-                    fill,
-                    style: "mix-blend-mode: soft-light",
-                    opacity: 0.25,
-                  },
                 }),
               ],
               props: {
@@ -350,6 +270,16 @@ const serializer = new XMLSerializer(),
       zIndex: -1,
     });
   };
+let metal = await fetch("./metal.png")
+  .then((response) => response.blob())
+  .then((blob) => {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+    });
+  });
 
 ((fn) => {
   if (document.readyState !== "loading") fn();
@@ -358,6 +288,7 @@ const serializer = new XMLSerializer(),
   Array.from(document.querySelectorAll(".metallicss")).forEach((elem) => {
     elem.style.boxShadow = "#00000030 1px 2px 2px, #00000020 2px 4px 4px";
     elem.style.color = "white";
+    elem.style.textShadow = "black 0 -0.5px 1px";
     elem.style.overflow = "hidden";
     elem.style.textRendering = "geometricPrecision";
     elem.style.transform = "translateZ(0)";
